51b33d07e891639caab66518dc23582f6e61bd42
Rodney Race <rodneyrace@gmail.com>
2019-12-27 10:40:35 -0500

    update

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 
 export default function Routes() {
   return (
     <Switch>
-        <Route path="/login" exact component={Login} />
+        <Route path="/" exact component={Login} />
         <Route path="/" exact component={Home} />
         { /* Finally, catch all unmatched routes */ }
-        <Route component={NotFound} />
     </Switch>
   );
 }
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Home.css";
 
+/*
+info followed by https://serverless-stack.com/chapters/create-a-login-page.html
+*/
 export default function Home() {
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
       </div>
     </div>
   );
